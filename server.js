6983d864f60520768a41d333d8ee76149eff8d27
maximusWhitaker <122586215+maximusWhitaker@users.noreply.github.com>
2024-06-27 00:57:56 +0000

    Added signup api

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/login', async (req, res, next) =>
-    {
+{
     // incoming: login, password
     // outgoing: id, firstName, lastName, error
     var error = '';
@@ -43,7 +43,50 @@ app.post('/api/login', async (req, res, next) =>
     }
     var ret = { id:id, firstName:fn, lastName:ln, error:''};
     res.status(200).json(ret);
-    });
+});
+
+app.post('/api/signup', async (req, res, next) => {
+    // incoming: firstname, lastname, login, password, email, phone
+    // outgoing: id, firstName, lastName, error
+
+    var error = '';
+    
+    const { login, password, firstname, lastname, phone, email } = req.body;
+
+    let newUser = { Login: login, Password: password, FirstName: firstname, LastName: lastname, Phone: phone, Email: email };
+
+    const db = client.db('COP4331');
+
+    //Checks to see if login or email is taken
+    const loginExists = await db.collection('Users').findOne({Login: req.body.login});
+    const emailExists = await db.collection('Users').findOne({Email: req.body.email});
+
+    if ( (loginExists && emailExists)){
+
+        console.log("User " + login + " and email " + email + " already exist!");
+        return res.status(409).send("Login and email already exist");
+
+    } else if (emailExists){
+
+        console.log("Email " + email + " already exists!");
+        return res.status(409).send("Email already exists");
+
+    } else if(loginExists){
+        
+        console.log("User " + login + " already exist!");
+        return res.status(409).send("Login already exists");
+
+    }else{
+        db.collection('Users').insertOne(newUser, function(err, res){
+            if (err) throw err;
+        });
+        const results = await db.collection('Users').find({Login: req.body.login}).toArray();
+        console.log("User " + login + " added!");
+        var ret = { _id: results[0]._id, firstName: firstname, lastName: lastname, error: '' };
+        res.status(200).json(ret);
+    }
+
+});
 
 
 app.listen(5001); // start Node + Express server on port 5001
