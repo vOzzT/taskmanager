b5b3515172b4fe1a37d03e5c955a44aa9c765469
Kensley Cadet <131932916+yeyadude7@users.noreply.github.com>
2024-07-01 13:38:28 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home.jsx'
+import Login from './Login.jsx';
+import SignUp from './SignUp.jsx';
+import Calendar from './Calendar.jsx';
 
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+    return (
+      <Router>
+        <Switch>
+
+          <Route exact path = "/">
+            <Home/>
+          </Route>
+
+          <Route path = "/login">
+            <Login/>
+          </Route>
+
+          <Route path = "/signup">
+            <SignUp/>
+          </Route>
+        
+          <Route path = "/calendar">
+            <Calendar/>
+          </Route>
+
+        </Switch>
+      </Router>
+    );
 }
 
-export default App;
+export default App
+
