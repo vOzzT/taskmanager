c8965de27c5db2462e9608f55f5245c756bd5355
Kensley Cadet <131932916+yeyadude7@users.noreply.github.com>
2024-07-08 18:28:36 -0400

    Create login.dart

---
+++ mobile/lib/pages/login.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobile/components/my_textfield.dart';
+
+
+final _formKey = GlobalKey<FormState>();
+
+class LoginFormScreen extends StatefulWidget {
+  const LoginFormScreen({super.key});
+
+  @override
+  State<LoginFormScreen> createState() => LoginPage();
+}
+
+
+class LoginPage extends State<LoginFormScreen> {
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool passToggle = true;
+  // sign user in method
+  void login() {}
+
+  void signUp() {}
+
+
+  String? validateUser(String? user) {
+
+    if (user!.isEmpty) {
+      return 'Please enter a username';
+    }
+    return null;
+  }
+
+  String? validatePass(String? pass) {
+
+    if (pass!.isEmpty) {
+      return 'Please enter a password';
+    }
+    return null;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView(
+        child: Center(
+          child: SizedBox(
+            width: 360,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 50),
+
+                  // logo
+                  const Icon(
+                    Icons.lock,
+                    size: 100,
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  // welcome back, you've been missed!
+                  Text(
+                    'Welcome back you\'ve been missed!',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 16,
+                    ),
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  // username textfield
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Username',
+                    obscureText: false,
+                    validator: validateUser,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  // password textfield
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                    validator: validatePass,
+
+                  ),
+
+                  const SizedBox(height: 10),
+
+
+
+                  const SizedBox(height: 25),
+
+                  // login button
+                  GestureDetector(
+                    onTap: () {
+                      _formKey.currentState!.validate();
+                    },
+                    child: Container(
+                      height: 50,
+
+                      margin: const EdgeInsets.symmetric(horizontal: 25),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+
+                      child: const Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  // or continue with
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text(
+                            'Not registered?',
+                            style: TextStyle(color: Colors.grey[700]),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 25),
+
+                  // sign up button
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: Container(
+                      height: 50,
+                      margin: const EdgeInsets.symmetric(horizontal: 25),
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+
+                      child: const Center(
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 50),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
