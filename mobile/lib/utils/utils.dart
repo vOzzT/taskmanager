73bfec4d05671caddac04e5b346805b173ab01d7
Kensley Cadet <131932916+yeyadude7@users.noreply.github.com>
2024-07-21 18:09:37 -0400

    Add files via upload

--- mobile/lib/utils/utils.dart
+++ mobile/lib/utils/utils.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
 void showSnackBar(BuildContext context, String text) {
@@ -21,6 +20,12 @@ void httpErrorHandle({
     case 200:
       onSuccess();
       break;
+    case 400:
+      showSnackBar(context, jsonDecode(response.body)['msg']);
+      break;
+    case 403:
+      showSnackBar(context, jsonDecode(response.body)['msg']);
+      break;
     case 409:
       showSnackBar(context, jsonDecode(response.body)['msg']);
       break;
@@ -30,7 +35,53 @@ void httpErrorHandle({
     case 411:
       showSnackBar(context, jsonDecode(response.body)['msg']);
       break;
+    case 500:
+      showSnackBar(context, jsonDecode(response.body)['msg']);
+      break;
     default:
       showSnackBar(context, response.body);
   }
+}
+
+class Utils {
+  static String toDateTime(DateTime dateTime) {
+    final date = DateFormat.yMMMEd().format(dateTime);
+    final time = DateFormat.Hm().format(dateTime);
+
+    return '$date $time';
+  }
+
+  static String toJsonDateTime(DateTime dateTime) {
+    final date = DateFormat("MM-dd-yyyy").format(dateTime);
+    final time = DateFormat("HH-mm-ss").format(dateTime);
+
+    return '$date-$time';
+  }
+
+  static String toDate(DateTime dateTime) {
+    final date = DateFormat.yMMMEd().format(dateTime);
+
+    return date;
+  }
+
+  static String toTime(DateTime dateTime) {
+    final time = DateFormat.Hm().format(dateTime);
+
+    return time;
+  }
+
+  static String toHour(DateTime dateTime) {
+    final time = DateFormat.jm().format(dateTime);
+
+    return time;
+  }
+  
+  static String toWeekDay(DateTime dateTime) {
+    final time = DateFormat('EEEE').format(dateTime); 
+
+    return time;
+  }
+
+  static DateTime removeTime(DateTime dateTime) =>
+    DateTime(dateTime.year, dateTime.month, dateTime.day);
 }
