2aab1661451a935827e6bb71e9804bd6d4388f0c
maximusWhitaker <122586215+maximusWhitaker@users.noreply.github.com>
2024-07-20 22:07:42 -0700

    Create ResetPage.js

---
+++ frontend/src/pages/ResetPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignUp from '../components/Reset';
+
+const ResetPage = () => {
+    return (
+        <div>
+            <Reset />
+        </div>
+    )
+}
+
+export default ResetPage;
