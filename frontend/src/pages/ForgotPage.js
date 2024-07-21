161371e73c8197028bdd9a824add33da4d2289fe
maximusWhitaker <122586215+maximusWhitaker@users.noreply.github.com>
2024-07-20 22:06:26 -0700

    Create ForgotPage.js

---
+++ frontend/src/pages/ForgotPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignUp from '../components/Forgot';
+
+const ForgetPage = () => {
+    return (
+        <div>
+            <Forgot />
+        </div>
+    )
+}
+
+export default ForgotPage;
